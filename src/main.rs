157b7b670326991ce8cd6476858e38eeddbdcb1e
Daiki48 <daiki48.engineer@gmail.com>
2023-07-17 00:59:55 +0900

    :wrench: Added about page

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use tera::Tera;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", routing::get(root));
+    let app = Router::new()
+        .route("/", routing::get(root))
+        .route("/about", routing::get(about_page));
     Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
@@ -11,12 +13,24 @@ async fn main() {
 }
 
 async fn root() -> Html<String> {
-    let tera = Tera::new("templates/*").unwrap();
+    let tera = Tera::new("templates/*.html").unwrap();
 
     let mut context = tera::Context::new();
-    context.insert("title", "Indexページです。");
-    context.insert("message", "メッセージです。");
+    context.insert("title", "axum-tera");
+    context.insert("page_title", "Index");
+    context.insert("message", "indexページでのメッセージです。");
 
     let output = tera.render("index.html", &context);
+    Html(output.unwrap())
+}
+
+async fn about_page() -> Html<String> {
+    let tera = Tera::new("templates/pages/*.html").unwrap();
+
+    let mut context = tera::Context::new();
+    context.insert("title", "axum-tera");
+    context.insert("page_title", "About");
+    context.insert("message", "aboutページでのメッセージです。");
+    let output = tera.render("about.html", &context);
     Html(output.unwrap())
 }
